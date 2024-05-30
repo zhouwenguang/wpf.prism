9cc0fe383cb61f087d30d7a8683a8727136c7f42
周文光 <zhouwenguangmail@163.com>
2024-05-30 21:51:29 +0800

    新增moduleA模块

---
+++ src/wpf.prism/wpf.moduleA/moduleAModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using wpf.moduleA.Views;
+
+namespace wpf.moduleA
+{
+    public class moduleAModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
